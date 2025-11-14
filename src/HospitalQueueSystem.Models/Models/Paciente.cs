0ee2dc849c40fec217118502ac7d0ea51d376d1e
Miguel <mike.csmf@gmail.com>
2025-11-13 18:38:31 -0600

    Primer commit:Creacion proyecto Desarrollo Web

---
+++ src/HospitalQueueSystem.Models/Models/Paciente.cs
@@ -0,0 +1,26 @@
+namespace HospitalQueueSystem.Models
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; } = string.Empty;
+        
+        [Required]
+        [StringLength(20)]
+        public string Identificacion { get; set; } = string.Empty;
+        
+        public DateTime FechaNacimiento { get; set; }
+        
+        [StringLength(10)]
+        public string Genero { get; set; } = string.Empty;
+        
+        [StringLength(200)]
+        public string Sintomas { get; set; } = string.Empty;
+        
+        public DateTime FechaRegistro { get; set; } = DateTime.UtcNow;
+    }
+}
